5aa1700926e23a1afa335177be789e853c48cb56
Tyler Briskie <tylerbriskie@gmail.com>
2018-04-28 14:26:26 -0600

    initial commit :+1:

---
+++ src/Header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="site-header">
+                <h5>Tyler B.</h5>
+            </header>
+        );
+    }
+}
+
+export default Header;
